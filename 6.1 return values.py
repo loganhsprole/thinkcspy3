ddbdefb7a5dd193421ed7dc6c1d2f1f1fc8db923
loganhsprole <logansprole@gmail.com>
2021-03-26 12:50:50 -0400

    commit

--- 6.1 return values.py
+++ 6.1 return values.py
@@ -2,3 +2,21 @@ biggest = max(3, 7, 2, 5)
 x = abs(3 - 11) + 10
 print(biggest)
 print(x)
+
+
+def area(radius):
+    return 3.14159 * radius * radius
+
+
+a = area(2)
+print(a)
+
+
+def absolute_value(x):
+    if x < 0:
+        return -x
+    return x
+
+
+b = absolute_value(-2)
+print(b)
