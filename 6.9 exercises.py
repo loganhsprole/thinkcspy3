593f830b01f3b09fafa3d02c14553b5f4c03c4dc
loganhsprole <logansprole@gmail.com>
2021-03-28 02:55:51 -0400

    commit

--- 6.9 exercises.py
+++ 6.9 exercises.py
@@ -20,6 +20,7 @@ assert turn_clockwise("W") == "N"
 assert turn_clockwise(42) == None
 assert turn_clockwise("rubbish") == None
 
+
 # Exercise 2. Convert integer number 0 to 6 into name of a day.
 
 
@@ -46,3 +47,55 @@ def day_name(day_num):
 assert day_name(3) == "Wednesday"
 assert day_name(6) == "Saturday"
 assert day_name(42) == None
+
+
+# Exercise 3: Convert name of a day into integer number 0 to 6.
+
+
+def day_num(day_name):
+    """Converts the name of a day into an integer number 0 to 6."""
+    if day_name == "Sunday":
+        return 0
+    elif day_name == "Monday":
+        return 1
+    elif day_name == "Tuesday":
+        return 2
+    elif day_name == "Wednesday":
+        return 3
+    elif day_name == "Thursday":
+        return 4
+    elif day_name == "Friday":
+        return 5
+    elif day_name == "Saturday":
+        return 6
+    else:
+        return None
+
+
+assert day_num("Friday") == 5
+assert day_num("Sunday") == 0
+assert day_num(day_name(3)) == 3
+assert day_name(day_num("Thursday")) == "Thursday"
+assert day_num("Halloween") == None
+
+
+# Exercise 4. Answer questions like "Today is Wednesday. I leave on holiday in 19 days time. What day will that be?"
+
+
+def day_add(day, delta):
+    """Takes a day name and a delta argument and returns a resulting day name."""
+    return day_name((day_num(day) + delta) % 7)
+
+
+assert day_add("Monday", 4) == "Friday"
+assert day_add("Tuesday", 0) == "Tuesday"
+assert day_add("Tuesday", 14) == "Tuesday"
+assert day_add("Sunday", 100) == "Tuesday"
+
+
+# Exercise 5. Does day_add work with negative deltas?
+
+
+assert day_add("Sunday", -1) == "Saturday"
+assert day_add("Sunday", -7) == "Sunday"
+assert day_add("Tuesday", -100) == "Sunday"
