122402a200265c2b2b11fbcec80ad0804709436e
loganhsprole <logansprole@gmail.com>
2021-03-27 20:50:29 -0400

    commit

--- 6.9 exercises.py
+++ 6.9 exercises.py
@@ -1,2 +1,21 @@
 # Exercise 1. Compass points.
 
+
+def turn_clockwise(point):
+	"""Takes a compass point as its parameters and returns the next compass point in the clockwise direction."""
+    if point == "N":
+        return "E"
+    elif point == "E":
+        return "S"
+    elif point == "S":
+        return "W"
+    elif point == "W":
+        return "N"
+    else:
+        return
+
+
+assert turn_clockwise("N") == "E"
+assert turn_clockwise("W") == "N"
+assert turn_clockwise(42) == None
+assert turn_clockwise("rubbish") == None
