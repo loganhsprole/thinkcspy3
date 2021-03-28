d83efc8f275f2670fe55b57515913e144f63777a
loganhsprole <logansprole@gmail.com>
2021-03-27 21:28:00 -0400

    commit

--- 6.9 exercises.py
+++ 6.9 exercises.py
@@ -12,10 +12,37 @@ def turn_clockwise(point):
     elif point == "W":
         return "N"
     else:
-        return
+        return None
 
 
 assert turn_clockwise("N") == "E"
 assert turn_clockwise("W") == "N"
 assert turn_clockwise(42) == None
 assert turn_clockwise("rubbish") == None
+
+# Exercise 2. Convert integer number 0 to 6 into name of a day.
+
+
+def day_name(day_num):
+    """Converts an integer number 0 to 6 into the name of a day."""
+    if day_num == 0:
+        return "Sunday"
+    elif day_num == 1:
+        return "Monday"
+    elif day_num == 2:
+        return "Tuesday"
+    elif day_num == 3:
+        return "Wednesday"
+    elif day_num == 4:
+        return "Thursday"
+    elif day_num == 5:
+        return "Friday"
+    elif day_num == 6:
+        return "Saturday"
+    else:
+        return None
+
+
+assert day_name(3) == "Wednesday"
+assert day_name(6) == "Saturday"
+assert day_name(42) == None
